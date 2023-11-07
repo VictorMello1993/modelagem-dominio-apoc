561e17251ccedde0662498488d960318ab2a8313
Victor Mello <victorsmello93@gmail.com>
2023-11-07 13:28:38 -0300

    project setup

---
+++ jest.config.js
@@ -0,0 +1,10 @@
+/** @type {import('ts-jest').JestConfigWithTsJest} */
+module.exports = {
+  preset: "ts-jest",
+  testEnvironment: "node",
+  testMatch: ["**/tests/**/*.spec.ts"],
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/src/$1"
+  },
+  // setupFiles: ["<rootDir>/tests/.env.ts"]
+};
---
+++ tests/index.spec.ts
@@ -0,0 +1,5 @@
+import {somar} from "@/index"
+
+test('Deve somar dois números corretamente', () => {
+  expect(somar(1, 2)).toBe(3)
+})
